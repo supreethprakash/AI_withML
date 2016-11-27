06febef9ccc7011341dc5a73af6a199ce9b3bcd8
Supreeth Keragodu Suryaprakash <skeragod@silo.soic.indiana.edu>
2016-11-26 22:40:20 -0500

    made it 4 spaces

--- part1/test.py
+++ part1/test.py
@@ -9,27 +9,27 @@ for path,dirs,files in os.walk(start_path):
 
 wordsList = list()
 
-specialCharacters = ['\n','\t', ' ', '\r']
+specialCharacters = ['\n','\t', ' ', '\r','']
 
 def readFile(fileName):
-  file = open(fileName, 'r')
-  lines = file.readlines()
-  for i in range(len(lines)):
-    if lines[i].isspace() == True:
-      return i, lines
+    file = open(fileName, 'r')
+    lines = file.readlines()
+    for i in range(len(lines)):
+        if lines[i].isspace() == True:
+            return i, lines
 
 def addToArray(lines):
-  for line in lines:
-    word = line.split(' ')
-    if word not in wordsList and word not in specialCharacters:
-      wordsList.append(word)
+    for line in lines:
+        word = line.split(' ')
+        if word not in wordsList and word not in specialCharacters:
+            wordsList.append(word)
 
 if __name__ == '__main__':
-  start_path = '/home/rkanchib/Downloads/AI/assignment 4/anupbhar-rkanchib-skeragod-a4/part1/train'
-  for path, dirs, files in os.walk(start_path):
-    for filename in files:
-      if filename != 'cmds':
-        x = os.path.join(path, filename)
-        index, Lines = readFile(x)
-  addToArray(Lines[index+1:len(Lines)])
-  print wordsList
+    start_path = 'train/spam/'
+    for path, dirs, files in os.walk(start_path):
+        for filename in files:
+            if filename != 'cmds':
+                x = os.path.join(path, filename)
+                index, Lines = readFile(x)
+                addToArray(Lines[index+1:len(Lines)])
+    print wordsList
