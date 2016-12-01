c35ab54fe07782c7825c87c0397f7f051aacb9c2
Supreeth Keragodu Suryaprakash <skeragod@silo.soic.indiana.edu>
2016-11-30 23:09:49 -0500

    top 1000 blunder fixed

--- part1/utilities.py
+++ part1/utilities.py
@@ -15,6 +15,6 @@ def readModelFile():
 	wordsInModelFile = readFile('modelFile.txt')
 	for eachLine in wordsInModelFile:
 		line = eachLine.split('\t')
-		bagOfWords.append((line[0], line[3]))
+		bagOfWords.append((line[0], line[4]))
 	bagOfWords.sort(key=lambda tup: tup[1])
 	return bagOfWords[-1000:]
