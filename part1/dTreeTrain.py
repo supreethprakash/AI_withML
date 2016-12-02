244ee4ea3886a11dd0182fe2c325b534d1c0c879
Supreeth Keragodu Suryaprakash <skeragod@silo.soic.indiana.edu>
2016-12-01 22:52:08 -0500

    removed files

--- part1/dTreeTrain.py
+++
@@ -1,4 +0,0 @@
-def divideset(rows, column, value):
-    set1 = [row for row in rows if row[column] >= value]
-    set2 = [row for row in rows if not row[column] >= value]
-    return (set1, set2)
--- part1/decisionTree.py
+++
@@ -1,9 +0,0 @@
-
-class decisionnode:
-  def __init__(self,col=-1,value=None,results=None,tb=None,fb=None):
-    self.col=col
-    self.value=value
-    self.results=results
-    self.tb=tb
-    self.fb=fb
-
